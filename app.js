a3448953f906ac56d00f6fb24628db9e3b2ddeea
Tejas Kedare <tejaskedare2@gmail.com>
2020-10-27 17:27:34 +0530

    adding list data in database is possible

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist",
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item",
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item",
+});
+
+const defaultItem = [item1, item2, item3];
 
 app.get("/", (req, res) => {
   let today = new Date();
@@ -16,21 +39,31 @@ app.get("/", (req, res) => {
     day: "numeric",
     month: "long",
   };
-
   let currentDay = today.toLocaleDateString("hi-IN", options);
 
-  res.render("list", { listTitle: currentDay, newListItems: items });
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItem, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("data has been inserted");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: currentDay, foundItems: foundItems });
+    }
+  });
 });
 
 app.post("/", (req, res) => {
-  let item = req.body.newItem;
-  if (req.body.list === "Work List") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
 });
 
 app.get("/work", (req, res) => {
